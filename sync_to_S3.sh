60a59287a9cef1e027535fe2d1f5074dc99df71e
Andruida <borus.benedek@gmail.com>
2023-02-24 15:16:03 +0100

    PHP változatban szükségtelen szkriptek

--- sync_to_S3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-shopt -s expand_aliases
-source ~/.bash_aliases
-
-cntb-s5cmd sync --exclude ".git/*" --delete . s3://webterv/
--- validate.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -i -v $PWD:/mnt -w /mnt cyb3rjak3/html5validator:latest-alpine html5validator --root .
