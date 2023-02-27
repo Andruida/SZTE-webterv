2e090ba4c87e5d9baf7ceb97c208e1107d928afc
Andruida <borus.benedek@gmail.com>
2023-02-27 15:08:45 +0100

    Login átformázása

--- backend/login.php
+++ backend/login.php
@@ -3,7 +3,7 @@
 session_start();
 
 if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-    header('Location: ../index.php');
+    header('Location: ../profil.php');
     exit();
 }
 
@@ -14,7 +14,7 @@ $password = $_POST['password'];
 if (isset($password) && !empty($password)) {
     $password = trim($password);
 } else {
-    header("Location: ../bejelentkezés.php?form=login&error=EmptyPassword&email=".urlencode($email));
+    header("Location: ../bejelentkezés.php?form=login&error=EmptyPassword&email=" . urlencode($email));
     echo "A jelszó mező nem lehet üres!";
     exit();
 }
@@ -23,33 +23,35 @@ if (isset($password) && !empty($password)) {
 if (isset($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $email = trim($email);
 } else {
-    header("Location: ../bejelentkezés.php?form=login&error=InvalidEmail&email=".urlencode($email));
+    header("Location: ../bejelentkezés.php?form=login&error=InvalidEmail&email=" . urlencode($email));
     echo "Az e-mail cím formátuma nem megfelelő.";
     exit();
 }
 
-require(__DIR__.'/conn.php');
+require(__DIR__ . '/conn.php');
 
 $sql = "SELECT * FROM users WHERE email = ?";
 
 $stmt = mysqli_prepare($conn, $sql);
 // mysqli_stmt_bind_param($stmt, 's', $email);
-$success = mysqli_stmt_execute($stmt, [ $email ]);
+$success = mysqli_stmt_execute($stmt, [$email]);
 $result = mysqli_stmt_get_result($stmt);
 
-if (mysqli_num_rows($result) == 1) {
-  $row = mysqli_fetch_assoc($result);
-  if (password_verify($password, $row['password'])) {
+if (mysqli_num_rows($result) === 0) {
+    header("Location: ../bejelentkezés.php?form=login&error=UserDoesntExist&email=" . urlencode($email));
+    echo "A felhasználónév nem található.";
+    exit();
+}
+
+$row = mysqli_fetch_assoc($result);
+
+if (password_verify($password, $row['password'])) {
     $_SESSION['email'] = $email;
     $_SESSION['id'] = $row["id"];
-    // setcookie('email', $email, time() + (86400 * 30), '/');
-    header("Location: ../index.php");
+    
+    header("Location: ../profil.php");
     echo "Sikeres bejelentkezés!";
-  } else {
-    header("Location: ../bejelentkezés.php?form=login&error=WrongPassword&email=".urlencode($email));
-    echo "Hibás jelszó!";
-  }
 } else {
-  header("Location: ../bejelentkezés.php?form=login&error=UserDoesntExist&email=".urlencode($email));
-  echo "A felhasználónév nem található.";
+    header("Location: ../bejelentkezés.php?form=login&error=WrongPassword&email=" . urlencode($email));
+    echo "Hibás jelszó!";
 }
